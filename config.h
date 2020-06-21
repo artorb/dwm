4c5969c515268b749f31f029989b92c0a89980be
ar <munkilla@gmail.com>
2020-06-21 15:12:33 +0200

    fix

--- config.h
+++ config.h
@@ -12,7 +12,7 @@ static const int smartgaps          = 0;        /* 1 means no outer gap when the
 static const int showbar            = 1;        /* 0 means no bar */
 static const int topbar             = 2;        /* 0 means bottom bar */
 static const int user_bh            = 30;        /* 0 means that dwm will calculate bar height, >= 1 means dwm will user_bh as bar height */
-static const char *fonts[]          = { "Iosevka Nerd Font:size=12", "Ubuntu Mono:size=12"};
+static const char *fonts[]          = { "Iosevka Nerd Font:size=13", "Ubuntu Mono:size=12"};
 static const char dmenufont[]       = "Iosevka Nerd Font:Bold:size=13";
 static const char col_gray1[]       = "#222222";
 static const char col_gray2[]       = "#444444";
@@ -99,6 +99,7 @@ static Key keys[] = {
 
     { MODKEY_ALT,             XK_F7,      spawn,      SHCMD("brightnessctl set 5%-") },
     { MODKEY_ALT,             XK_F8,      spawn,      SHCMD("brightnessctl set 5%+") },
+    { MODKEY_ALT,             XK_F10,     spawn,      SHCMD("scrot") },
     { MODKEY_ALT,             XK_F5,      spawn,      SHCMD("mpc toggle") },
     { MODKEY_ALT,             XK_F6,      spawn,      SHCMD("mpc next") },
     { MODKEY_ALT,             XK_F4,      spawn,      SHCMD("mpc prev") },
