09dc168eb9afed91ec22c3827ba3c64fe51316a6
ar <munkilla@gmail.com>
2020-07-01 00:33:22 +0200

    fix

--- config.h
+++ config.h
@@ -83,7 +83,7 @@ static const Layout layouts[] = {
 
 #define TAGKEYS(KEY,TAG) \
 	{ MODKEY,                       KEY,      view,           {.ui = 1 << TAG} }, \
-	{ MODKEY_RCTR,                       KEY,      view,           {.ui = 1 << TAG} }, \
+	{ MODKEY_RCTR,                  KEY,      view,           {.ui = 1 << TAG} }, \
 	{ MODKEY|ControlMask,           KEY,      toggleview,     {.ui = 1 << TAG} }, \
 	{ MODKEY|ShiftMask,             KEY,      tag,            {.ui = 1 << TAG} }, \
 	{ MODKEY|ControlMask|ShiftMask, KEY,      toggletag,      {.ui = 1 << TAG} },
