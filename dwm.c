1fefd5ebaba7d211bd2c6d037be82694d2840561
munkilla <munkilla@gmail.com>
2020-09-09 15:17:14 +0000

    patchd

--- dwm.c
+++ dwm.c
@@ -789,6 +789,10 @@ drawbar(Monitor *m)
  	//int boxs = drw->fonts->h / 9; square icon tag
 	int boxw = drw->fonts->h / 6 + 2;
 	unsigned int i, occ = 0, urg = 0;
+    char *ts = stext;
+    char *tp = stext;
+    int tx = 0;
+    char ctmp;
 	Client *c;
 
 	/* draw status first so it can be overdrawn by tags later */
@@ -796,6 +800,17 @@ drawbar(Monitor *m)
 		drw_setscheme(drw, scheme[SchemeStatus]);
 		sw = TEXTW(stext) - lrpad + 2; /* 2px right padding */
 		drw_text(drw, m->ww - sw, 0, sw, bh, 0, stext, 0);
+        while(1) {
+            if ((unsigned int) *ts > LENGTH(colors)) {ts++; continue;}
+            ctmp = *ts;
+            *ts = '\0';
+            drw_text(drw, m->ww - sw + tx, 0, sw - tx, bh, 0, tp, 0);
+            tx += TEXTW(tp) - lrpad;
+            if (ctmp == '\0') { break; }
+            drw_setscheme(drw, scheme[ (unsigned int) (ctmp-1)]);
+            *ts = ctmp;
+            tp = ++ts;
+        }
 	}
 
 	for (c = m->clients; c; c = c->next) {
